8f52b5ac024495629306c3fd79b5cffbb3425edf
NazmulHasan0 <nazmul552123@gmail.com>
2021-11-04 16:21:03 +0600

    init

---
+++ android/app/src/main/kotlin/com/example/More/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.More
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[300],
+      body: SafeArea(
+        bottom: true,
+        child: Center(
+          child: Text('hi'),
+        ),
+      ),
+    );
+  }
+}
